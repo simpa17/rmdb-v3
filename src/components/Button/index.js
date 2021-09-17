3343e4c997a48e9f2514205709c09e3a46e527f4
simpa17 <giandon.stella@gmail.com>
2021-09-17 11:51:39 +0700

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+// Style
+import { Wrapper } from "./Button.style";
+
+const Button = ({ text, callback }) => (
+	<Wrapper type="button" onClick={callback}>
+		{text}
+	</Wrapper>
+);
+
+Button.propTypes = {
+	text: PropTypes.string,
+	callback: PropTypes.func,
+};
+
+export default Button;
